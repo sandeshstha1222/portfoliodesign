58a2b9b9533668e9283bd69b1a5a2aa27e8ae148
sandeshstha1222 <sananddesh1029@gmail.com>
2023-03-16 11:53:07 +0545

    Responsive Header Created

--- src/components/header/Socials.js
+++ src/components/header/Socials.js
@@ -4,20 +4,22 @@ import { SiLinkedin, SiGithub, SiFacebook, SiInstagram } from "react-icons/si";
 
 const Socials = () => {
   return (
-    <div className="Social-Links">
-      <a>
-        <SiLinkedin size={30} />
-      </a>
+    <div className="Socials">
+      <div className="Social-Links">
+        <a>
+          <SiLinkedin size={30} />
+        </a>
 
-      <a>
-        <SiGithub size={30} />
-      </a>
-      <a>
-        <SiFacebook size={30} />
-      </a>
-      <a>
-        <SiInstagram size={30} />
-      </a>
+        <a>
+          <SiGithub size={30} />
+        </a>
+        <a>
+          <SiFacebook size={30} />
+        </a>
+        <a>
+          <SiInstagram size={30} />
+        </a>
+      </div>
     </div>
   );
 };
